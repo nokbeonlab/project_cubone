87fdafeb009fc0156c40ec392e7317b668280ead
Nokboen_Jung <nokbeonlab@gmail.com>
2021-05-25 00:26:30 +0900

    로컬 및 글로벌 name/email 설정 문제로 github 잔디 관리 안되던 것 해결

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -7,7 +7,6 @@ public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
 
-
     @Override
     public void save(Member member) {
         store.put(member.getId(), member);
